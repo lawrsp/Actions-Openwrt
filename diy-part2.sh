05f891708807c86957d20e29a9eab50461024c6c
lawrsp <7957003+lawrsp@users.noreply.github.com>
2023-01-13 00:48:35 +0800

    add transmission

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,18 +13,39 @@
 ### need some openwrt packages:
 
 
+function git_clone() {
+    git clone --depth=1 $1 $2
+    if [ "$?" != 0 ]; then
+        echo "error on $1"
+        pid="$( ps -q $$ )"
+        kill $pid
+    fi
+}
 
-# Xray-core
-git clone --depth=1  https://git.openwrt.org/feed/packages.git openwrtpackages
+git_clone https://git.openwrt.org/feed/packages.git openwrtpackages
+
+
+# 没有 xray-core
+rm -rf ./feeds/packages/net/xray-core
 mv -n openwrtpackages/net/xray-core ./feeds/packages/net/
 
+# lede 的 transmission 没有web管理
+rm -rf ./feeds/packages/net/transmission
+mv -n openwrtpackages/net/transmission ./feeds/packages/net/
+
 rm -rf openwrtpackages
+
+sed -i 's/transmission-daemon-openssl/transmission-daemon/' feeds/luci/applications/luci-app-transmission/Makefile
+sed -i 's/transmission-daemon-openssl/transmission-daemon/' feeds/packages/net/transmission-web-control/Makefile
+
 
 # reinstall feeds
 ./scripts/feeds install -a
 
-cd package/feeds/pacakges
-ln -sf ../../../feeds/packages/net/xray-core .
+
+cd package/feeds/packages
+rm -rf xray-core
+ln -sf ../../../feeds/packages/net/xray-core xray-core
 cd -
 
 ### end openwrt packages
